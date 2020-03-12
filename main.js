7aabf8a94bc56ff240172cf77a0a693fc2edb0a9
PiotrGasiorek <piotrjgasiorek@gmail.com>
2020-03-12 02:11:10 +0100

    Changes implemented to fix scrolls bugs

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ let links = document.querySelector('.nav__right').innerHTML;
 let sideNav = document.querySelector('.sideNav');
 let darkOverlay = document.querySelector('.darkOverlay');
 let body = document.querySelector('body');
-let html = document;
 
 sideNav.innerHTML += links;
 
-document.querySelector('.sideNav__btn').addEventListener("click", () => {
-    sideNav.classList.remove('showSideNav');
-    darkOverlay.style.display = 'none';
-    toggleScroll('on');
-})
+function toggleScroll(turn) {
+    if(turn === 'on'){
+        body.style.overflow = 'auto';
+    }else{
+        body.style.overflow = 'hidden';
+    }
+}
 
-window.addEventListener("resize", function() {
+function checkIfSmallDevice() {
     if(self.innerWidth < 736){
         this.document.querySelector('.nav__right').innerHTML = "<button class='btnCircle nav__btn btnCircle--open'></button>";
         this.document.querySelector('.nav__btn').addEventListener('click', () => {
@@ -20,19 +21,21 @@ window.addEventListener("resize", function() {
             darkOverlay.style.display = 'block';
             toggleScroll('off');
         })
-    }else{sideNav.classList.remove('showSideNav');
-        darkOverlay.style.display = 'none';
-        this.document.querySelector('.nav__right').innerHTML = links;
-    }
-
-})
-
-toggleScroll = (turn) => {
-    if(turn === 'on'){
-        body.style.overflow = 'auto';
-        body.style.position = 'fixed';
+        document.querySelector('.sideNav__btn').addEventListener("click", () => {
+            sideNav.classList.remove('showSideNav');
+            darkOverlay.style.display = 'none';
+            body.style.overflow = 'auto';
+        })
     }else{
-        body.style.overflow = 'hidden';
-        body.style.position = 'static';
+        sideNav.classList.remove('showSideNav');
+        darkOverlay.style.display = 'none';
+        document.querySelector('.nav__right').innerHTML = links;
+        toggleScroll('on');
     }
 }
+
+checkIfSmallDevice();
+
+window.addEventListener("resize", function() {
+    checkIfSmallDevice();
+})
